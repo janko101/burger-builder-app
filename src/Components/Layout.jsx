54f6df67b43ac56b556a1af80f21c9a79b95f4b4
Janko <mailodjanka@gmail.com>
2020-05-25 10:38:34 +0200

    Adds a styling module for Layout component

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import classes from "./Layout.module.css";
 
 const Layout = (props) => {
   return (
     <>
       <div>Toolbar, Sidedrawer, Backdrop</div>
-      <main>{props.children}</main>
+      <main className={classes.Content}>{props.children}</main>
     </>
   );
 };

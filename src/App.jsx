37dc77ee1bb56d32589f05af5bb0ece5f01a84c4
Janko <mailodjanka@gmail.com>
2020-05-25 09:39:46 +0200

    Prepares global styling, imports the open sans from google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
-    <div className="App">
+    <div>
       <h1>Let's build some burgers!</h1>
     </div>
     )

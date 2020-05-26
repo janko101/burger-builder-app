1298221e75aa4075d279052b82e084bc0fffc355
Janko <mailodjanka@gmail.com>
2020-05-26 10:51:07 +0200

    Imports Ingredient Component to the Burger one and renders a juicy burger on the screen

--- src/Components/Burger.jsx
+++ src/Components/Burger.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import classes from './Burger.module.css'
+import Ingredient from './Ingredient';
 
 const Burger = () => {
   return (
-    <div>
-      
+    <div className={classes.Burger}>
+      <Ingredient type='bread-top'/>
+      <Ingredient type='salad'/>
+      <Ingredient type='bacon'/>
+      <Ingredient type='cheese'/>
+      <Ingredient type='meat'/>
+      <Ingredient type='bread-bottom'/>
     </div>
   );
 }

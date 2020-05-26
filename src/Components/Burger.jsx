88e457dcda2f07e0c1874195191741fde7dece75
Janko <mailodjanka@gmail.com>
2020-05-26 12:05:56 +0200

    Adds a method to dynamicly put ingredients into the burger

--- src/Components/Burger.jsx
+++ src/Components/Burger.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import classes from './Burger.module.css'
-import Ingredient from './Ingredient';
+import React from "react";
+import classes from "./Burger.module.css";
+import Ingredient from "./Ingredient";
 
-const Burger = () => {
+const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <Ingredient key={igKey + i} type={igKey} />;
+    });
+  });
   return (
     <div className={classes.Burger}>
-      <Ingredient type='bread-top'/>
-      <Ingredient type='salad'/>
-      <Ingredient type='bacon'/>
-      <Ingredient type='cheese'/>
-      <Ingredient type='meat'/>
-      <Ingredient type='bread-bottom'/>
+      <Ingredient type="bread-top" />
+      {transformedIngredients}
+      <Ingredient type="bread-bottom" />
     </div>
   );
-}
+};
 
 export default Burger;
--- src/Components/BurgerBuilder.jsx
+++ src/Components/BurgerBuilder.jsx
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Burger from './Burger'
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 2,
+      cheese: 2,
+      meat: 1
+    }
+  }
   render() {
     return (
       <>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </>
     );

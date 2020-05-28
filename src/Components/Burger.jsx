19232159f1369d6d97008bff58d240a6c40c35e1
Janko <mailodjanka@gmail.com>
2020-05-28 13:38:24 +0200

    Sets ingredients initial state to 0, displays a paragraph to add ingredients if the length is 0

--- src/Components/Burger.jsx
+++ src/Components/Burger.jsx
@@ -3,11 +3,18 @@ import classes from "./Burger.module.css";
 import Ingredient from "./Ingredient";
 
 const Burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <Ingredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <Ingredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
   return (
     <div className={classes.Burger}>
       <Ingredient type="bread-top" />

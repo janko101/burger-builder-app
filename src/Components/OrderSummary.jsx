51d3498aed2ed960a3d13b0a179a04cd43ec6369
Janko <mailodjanka@gmail.com>
2020-06-01 13:23:43 +0200

    Adds inline styling to capitalize ingredients firts letter in the OrderSummary

--- src/Components/OrderSummary.jsx
+++ src/Components/OrderSummary.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const OrderSummary = (props) => {
-  const ingredientsSummary = Object.keys(props.ingredients)
-  .map(igKey => {
-    return <li>{igKey}: {props.ingredients[igKey]}</li>
-  })
+  const ingredientsSummary = Object.keys(props.ingredients).map((igKey) => {
+    return (
+      <li>
+        <span style={{ textTransform: "capitalize" }}>{igKey}: </span>
+        {props.ingredients[igKey]}
+      </li>
+    );
+  });
 
   return (
     <>
       <h3>Order Summary</h3>
       <p>You ordered a delicous Burger with following ingredients:</p>
-      <ul>
-        {ingredientsSummary}
-      </ul>
+      <ul>{ingredientsSummary}</ul>
+      <p>Continue to Checkout?</p>
     </>
   );
-}
+};
 
 export default OrderSummary;

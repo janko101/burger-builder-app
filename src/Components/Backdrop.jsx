7e553a4b325c93c25489324cb4b1728f9c04078e
Janko <mailodjanka@gmail.com>
2020-06-01 14:10:09 +0200

    Adds styling to Backdrop component

--- src/Components/Backdrop.jsx
+++ src/Components/Backdrop.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './Backdrop.module.css';
 
 const Backdrop = (props) => {
-  props.show ? <div></div> : null
+  props.show ? <div className={classes.Backdrop}></div> : null
 }
 
 export default Backdrop;

477bb6c7810014699a51b3ab36b8e684dc4a6857
Janko <mailodjanka@gmail.com>
2020-06-03 09:53:33 +0200

    Adds dynamic styling to the button depending on a button type

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import classes from "./Button.module.css";
 
 const Button = (props) => (
-  <button>{props.children}</button>
-)
+  <button
+    className={[classes.Button, classes[props.buttonType]].join(' ')}
+    onClick={props.clicked}
+  >
+    {props.children}
+  </button>
+);
 
-export default Button
+export default Button;

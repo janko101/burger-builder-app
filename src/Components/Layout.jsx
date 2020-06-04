b6b038620e9a34ef96c34b686247ae2b2ebd994e
Janko <mailodjanka@gmail.com>
2020-06-04 11:11:46 +0200

    Adds css module styling for a Sidedrawer components and imports the component in the Layout comp

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "./Toolbar";
+import Sidedrawer from "./Sidedrawer";
 
 const Layout = (props) => {
   return (
     <>
       <Toolbar />
+      <Sidedrawer />
       <main className={classes.Content}>{props.children}</main>
     </>
   );
--- src/Components/Sidedrawer.jsx
+++ src/Components/Sidedrawer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Logo from './Logo';
 import NavigationItems from './NavigationItems';
+import classes from './Sidedrawer.module.css'
 
 const Sidedrawer = (props) => {
   return (
-    <div>
+    <div className={classes.Sidedrawer}>
       <Logo />
       <nav>
         <NavigationItems />

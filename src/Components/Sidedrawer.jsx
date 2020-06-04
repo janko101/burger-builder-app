316b5bb8bab3b4c72cab2d30ef3ab767629bcc14
Janko <mailodjanka@gmail.com>
2020-06-04 11:24:57 +0200

    Refactores to use css module to adjust logo's height in Toolbar and in Sidedrawer

--- src/Components/Sidedrawer.jsx
+++ src/Components/Sidedrawer.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Logo from './Logo';
-import NavigationItems from './NavigationItems';
-import classes from './Sidedrawer.module.css'
+import React from "react";
+import Logo from "./Logo";
+import NavigationItems from "./NavigationItems";
+import classes from "./Sidedrawer.module.css";
 
 const Sidedrawer = (props) => {
   return (
     <div className={classes.Sidedrawer}>
-      <Logo height="11%"/>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
       <nav>
         <NavigationItems />
       </nav>
     </div>
   );
-}
+};
 
 export default Sidedrawer;
--- src/Components/Toolbar.jsx
+++ src/Components/Toolbar.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import classes from './Toolbar.module.css'
-import Logo from './Logo';
-import NavigationItems from './NavigationItems'
+import React from "react";
+import classes from "./Toolbar.module.css";
+import Logo from "./Logo";
+import NavigationItems from "./NavigationItems";
 
 const Toolbar = () => {
   return (
     <header className={classes.Toolbar}>
       <div>Menu</div>
-      <Logo height="80%"/>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
       <nav>
         <NavigationItems />
       </nav>
     </header>
   );
-}
+};
 
 export default Toolbar;

d4611582750a45c8bf08dc5230f1af3b8f28ed19
Janko <mailodjanka@gmail.com>
2020-06-18 11:11:39 +0200

    Imports a backdrop component into the Sidedrawer components and sets props show to true

--- src/Components/Sidedrawer.jsx
+++ src/Components/Sidedrawer.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import Logo from "./Logo";
 import NavigationItems from "./NavigationItems";
 import classes from "./Sidedrawer.module.css";
+import Backdrop from "./Backdrop";
 
 const Sidedrawer = (props) => {
   return (
-    <div className={classes.Sidedrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <>
+      <Backdrop show/>
+      <div className={classes.Sidedrawer}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </>
   );
 };
 

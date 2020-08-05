37c60209faef181d292501d36da669061fa50dcc
Janko <mailodjanka@gmail.com>
2020-08-05 11:58:16 +0200

    Changes OrderSummary back to func compoenent because point is proven

--- src/Components/OrderSummary.jsx
+++ src/Components/OrderSummary.jsx
@@ -1,39 +1,43 @@
 import React, { Component } from "react";
 import Button from "./Button";
 
-class OrderSummary extends Component {
-  render() {
-    const ingredientsSummary = Object.keys(this.props.ingredients).map((igKey) => {
+const OrderSummary = (props) => {
+  const ingredientsSummary = Object.keys(props.ingredients).map(
+    (igKey) => {
       return (
         <li id={igKey} key={igKey}>
           <span style={{ textTransform: "capitalize" }}>{igKey}: </span>
-          {this.props.ingredients[igKey]}
+          {props.ingredients[igKey]}
         </li>
       );
-    });
-    return (
-      <>
-        <h3 id="order-summary">Order Summary</h3>
-        <p>You ordered a delicous Burger with following ingredients:</p>
-        <ul>{ingredientsSummary}</ul>
-        <p id="price">
-          To Pay:
-          <strong> {this.props.totalPrice.toFixed(2)} SEK</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-        <Button id="cancel" buttonType="Danger" clicked={this.props.cancelPurchase}>
-          CANCEL
-        </Button>
-        <Button
-          id="confirm"
-          buttonType="Success"
-          clicked={this.props.confirmPurchase}
-        >
-          CONFIRM
-        </Button>
-      </>
-    );
-  }
-}
+    }
+  );
+  return (
+    <>
+      <h3 id="order-summary">Order Summary</h3>
+      <p>You ordered a delicous Burger with following ingredients:</p>
+      <ul>{ingredientsSummary}</ul>
+      <p id="price">
+        To Pay:
+        <strong> {props.totalPrice.toFixed(2)} SEK</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button
+        id="cancel"
+        buttonType="Danger"
+        clicked={props.cancelPurchase}
+      >
+        CANCEL
+      </Button>
+      <Button
+        id="confirm"
+        buttonType="Success"
+        clicked={props.confirmPurchase}
+      >
+        CONFIRM
+      </Button>
+    </>
+  );
+};
 
 export default OrderSummary;

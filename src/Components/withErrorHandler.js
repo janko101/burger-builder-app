a72d22a3f9ad8298e2b07b31b3467d35430da4b9
Janko <mailodjanka@gmail.com>
2020-08-10 13:48:40 +0200

    Puts prefix UNSAFE_ in front of componentWillMount lifecycle method as React depreciated this method and setState should not be used here anymore

--- src/Components/withErrorHandler.js
+++ src/Components/withErrorHandler.js
@@ -7,7 +7,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null,
     };
 
-    componentWillMount() {
+    UNSAFE_componentWillMount() {
       this.requestInterceptors = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;

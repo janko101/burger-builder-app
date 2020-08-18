568c138b5b765cd2f046aa939517befee2893ff6
Janko <mailodjanka@gmail.com>
2020-08-18 15:00:40 +0200

    Adds styling to CheckoutSummary comp using css modules

--- src/Components/CheckoutSummary.jsx
+++ src/Components/CheckoutSummary.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Burger from './Burger'
 import Button from './Button';
+import classes from './CheckoutSummary.module.css'
 
 const CheckoutSummary = (props) => {
   return (
-    <div>
+    <div className={classes.CheckoutSummary}>
       <h1>Hope it's Burgerlicious :))</h1>
       <div style={{height: '300px', width: '300px', margin: 'auto'}}>
         <Burger ingredients={props.ingredients}/>

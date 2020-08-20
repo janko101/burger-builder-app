3ab285b56a493ad19bb9361f77589df04d4db455
Janko <mailodjanka@gmail.com>
2020-08-20 14:28:21 +0200

    Makes a class active using activeClassName prop for NavLink

--- src/Components/NavigationItems.jsx
+++ src/Components/NavigationItems.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 import classes from "./NavigationItems.module.css";
 
 const NavigationItems = () => (
   <ul className={classes.NavigationItems}>
     <li className={classes.NavigationItem}>
-      <NavLink to="/">Burger Builder</NavLink>
-      <NavLink to="/orders">Orders</NavLink>
+      <NavLink to="/" exact activeClassName={classes.active}>
+        Burger Builder
+      </NavLink>
+      <NavLink to="/orders" activeClassName={classes.active}>
+        Orders
+      </NavLink>
     </li>
   </ul>
 );

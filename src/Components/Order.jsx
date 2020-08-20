7d2f532856ed363446def4ba144c90b795ccf642
Janko <mailodjanka@gmail.com>
2020-08-20 14:06:59 +0200

    Adds Orders a class based component and Order a func comp

---
+++ src/Components/Order.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Order = () => {
+  return (
+    <div>
+      <p>Ingedients: </p>
+      <p>
+        Price: <strong>80 SEK</strong>{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Order;

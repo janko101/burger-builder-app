8c1227fbe6d0d1aa6f29cd94fd728ff38a3ed56d
Janko <mailodjanka@gmail.com>
2020-10-12 14:32:29 +0200

    Adds orders reducer to set global state for orders and loading

---
+++ src/store/reducers/order.js
@@ -0,0 +1,30 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initialState = {
+  orders: [],
+  loading: false,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.BURGER_ORDER_SUCCESS:
+      const newOrder = {
+        ...action.orderData,
+        id: action.orderId,
+      };
+      return {
+        ...state,
+        loading: false,
+        orders: state.orders.concat(newOrder),
+      };
+    case actionTypes.BURGER_ORDER_FAILED:
+      return {
+        ...state,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;

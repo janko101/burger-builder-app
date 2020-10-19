672cc3c237f47d8e0c4d8bb794f1514174c089e8
Janko <mailodjanka@gmail.com>
2020-10-19 10:06:06 +0200

    Adds controls for email and password in the login form

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,6 +1,37 @@
 import React, { Component } from "react";
 
 class Auth extends Component {
+  state = {
+    email: {
+      elementType: "input",
+      elementConfig: {
+        type: "email",
+        placeholder: "Your Email",
+      },
+      value: "",
+      validation: {
+        required: true,
+        hasEmailFormat: true,
+      },
+      valid: false,
+      touched: false,
+    },
+    password: {
+      elementType: "input",
+      elementConfig: {
+        type: "password",
+        placeholder: "Password",
+      },
+      value: "",
+      validation: {
+        required: true,
+        minLength: 6
+      },
+      valid: false,
+      touched: false,
+    },
+
+  }
   render() {
     return (
       <div>

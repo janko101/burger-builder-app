d60c09c7bd4df79092c6d98a26719ae45dc68585
Janko <mailodjanka@gmail.com>
2020-10-27 10:31:37 +0100

    Adds authReducer for storing the token, sets the initial state and connects the action types

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,38 @@
+import * as actionTypes from "../actions/actionTypes";
+import { UpdateObject } from "../utility";
+
+const initialState = {
+  token: null,
+  userId: null,
+  error: null,
+  loading: false,
+};
+
+const authStart = (state, action) => {
+  return UpdateObject(state, { error: null, loading: true });
+};
+
+const authSuccess = (state, action) => {
+  return UpdateObject(state, {
+    token: action.tokenId,
+    userId: action.userId,
+    error: null,
+  });
+};
+
+const authFailed = (state, action) => {
+  return UpdateObject(state, { error: action.error });
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.AUTH_START:
+      return authStart(state, action);
+    case actionTypes.AUTH_SUCCESS:
+      return authSuccess(state, action);
+    case actionTypes.AUTH_FAILED:
+      return authFailed(state, action);
+    default:
+      return state;
+  }
+};

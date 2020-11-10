c145205cb2e37315ca3dc6cfc22d6f4681538c2d
Janko <mailodjanka@gmail.com>
2020-11-10 14:23:44 +0100

    Adds userId in querry params when fetching orders, so to only get orders with that userId

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -62,11 +62,12 @@ export const fetchOrdersFailed = (error) => {
   };
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
   return (dispatch) => {
-    dispatch(fetchOrdersStart())
+    dispatch(fetchOrdersStart());
+    const queryParams = "?auth=" + token + '&orderBy=&"userId"equalTo="' + userId + '"';
     axios
-      .get("/orders.json?auth=" + token)
+      .get("/orders.json" + queryParams)
       .then((res) => {
         const fetchedOrders = [];
         for (let key in res.data) {

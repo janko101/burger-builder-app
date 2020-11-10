c7eb2e6abb1eace3ad8bfbe171b9d2c16950a1b1
Janko <mailodjanka@gmail.com>
2020-11-10 13:50:21 +0100

    Maps state of authenticated to props in App comp to be able to guard certain routes if not authenticated

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,46 @@
 import React, { Component } from "react";
-import { connect } from "react-redux"
-import { Route } from "react-router-dom"
+import { connect } from "react-redux";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./Components/Layout";
 import BurgerBuilder from "./Components/BurgerBuilder";
 import Checkout from "./Components/Checkout";
 import Orders from "./Components/Orders";
-import Auth from "./Components/Auth"
-import Logout from "./Components/Logout"
-import * as actions from "./store/actions/index"
+import Auth from "./Components/Auth";
+import Logout from "./Components/Logout";
+import * as actions from "./store/actions/index";
 
 class App extends Component {
   componentDidMount() {
-    this.props.onAutoSignUp()
+    this.props.onAutoSignUp();
   }
+
   render() {
     return (
       <div>
-        <Layout> 
-          <Route path="/" exact component={BurgerBuilder}/> 
-          <Route path="/checkout" component={Checkout}/> 
-          <Route path="/orders" component={Orders}/> 
-          <Route path="/auth" component={Auth}/> 
-          <Route path="/Logout" component={Logout}/> 
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={BurgerBuilder} />
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
+            <Route path="/Logout" component={Logout} />
+          </Switch>
         </Layout>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapStateToProps = (state) => {
   return {
-    onAutoSignUp: () => dispatch(actions.authCheckState())
-  }
-}
+    isAuth: state.auth.token !== null,
+  };
+};
 
-export default connect(null, mapDispatchToProps)(App);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAutoSignUp: () => dispatch(actions.authCheckState()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);

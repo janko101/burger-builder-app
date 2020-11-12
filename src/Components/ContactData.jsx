a7acac20fd8fe22fe14189432a53b056cebc74db
Janko <mailodjanka@gmail.com>
2020-11-12 11:56:50 +0100

    Uses updateObject utility in inputHandler method in ContactData comp

--- src/Components/ContactData.jsx
+++ src/Components/ContactData.jsx
@@ -7,6 +7,7 @@ import Input from "./Input";
 import withErrorHandler from "./withErrorHandler";
 import axios from "../axios-orders";
 import * as actionCreators from "../store/actions/index";
+import { UpdateObject } from "../shared/utility";
 
 class ContactData extends Component {
   state = {
@@ -105,9 +106,9 @@ class ContactData extends Component {
       ingredients: this.props.ings,
       price: this.props.price,
       orderData: formData,
-      userId: this.props.userId
+      userId: this.props.userId,
     };
-    this.props.onOrderBurger(order, this.props.token)
+    this.props.onOrderBurger(order, this.props.token);
   };
 
   checkValidity(value, rules) {
@@ -128,20 +129,21 @@ class ContactData extends Component {
   }
 
   inputHandler = (event, inputIdentifier) => {
-    const updatedOrderForm = {
-      ...this.state.orderForm,
-    };
-
-    const updatedFormElement = {
-      ...updatedOrderForm[inputIdentifier],
-    };
-    updatedFormElement.value = event.target.value;
-    updatedFormElement.valid = this.checkValidity(
-      updatedFormElement.value,
-      updatedFormElement.validation
+    const updatedFormElement = UpdateObject(
+      this.state.orderForm[inputIdentifier],
+      {
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.orderForm[inputIdentifier].validation
+        ),
+        touched: true,
+      }
     );
-    updatedFormElement.touched = true;
-    updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+    const updatedOrderForm = UpdateObject(this.state.orderForm, {
+      [inputIdentifier]: updatedFormElement,
+    });
 
     let isFormValid = true;
     for (let inputIdentifier in updatedOrderForm) {
@@ -197,7 +199,7 @@ const mapStateToProps = (state) => {
     price: state.burgerReducer.totalPrice,
     loading: state.order.loading,
     token: state.auth.token,
-    userId: state.auth.userId
+    userId: state.auth.userId,
   };
 };
 

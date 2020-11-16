c98d56fdb82520e284ba6b7cfd12ac9cb0dde6e3
Janko <mailodjanka@gmail.com>
2020-11-16 15:03:36 +0100

    Adds a test to check if token and userId are stored after loging in

--- src/tests/auth.test.js
+++ src/tests/auth.test.js
@@ -1,14 +1,38 @@
-import reducer from "../store/reducers/auth"
-import * as actionTypes from "../store/actions/actionTypes"
+import reducer from "../store/reducers/auth";
+import * as actionTypes from "../store/actions/actionTypes";
 
-describe('auth reducer', () => {
-  it('should return initial state', () => {
+describe("auth reducer", () => {
+  it("should return initial state", () => {
     expect(reducer(undefined, {})).toEqual({
       token: null,
       userId: null,
       error: null,
       loading: false,
       authRedirectPath: "/",
-    })
-  }); 
+    });
+  });
+  it("should store the token and userId when authenticated", () => {
+    expect(
+      reducer(
+        {
+          token: null,
+          userId: null,
+          error: null,
+          loading: false,
+          authRedirectPath: "/",
+        },
+        {
+          type: actionTypes.AUTH_SUCCESS,
+          tokenId: "some-token",
+          userId: "some-userId",
+        }
+      )
+    ).toEqual({
+      token: "some-token",
+      userId: "some-userId",
+      error: null,
+      loading: false,
+      authRedirectPath: "/",
+    });
+  });
 });
